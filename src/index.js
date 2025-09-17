6df5c8e1b3a9b4e56c670cf3374691b04b8eb21c
chiraglokhande04 <chiraglokhande14@gmail.com>
2025-09-18 03:17:07 +0530

    adding cors origin

--- src/index.js
+++ src/index.js
@@ -17,9 +17,13 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin:[ "http://localhost:5173","rag-chatbot-frontend-mu.vercel.app"], // or "*" to allow all
+  origin: [
+    "http://localhost:5173",
+    "https://rag-chatbot-frontend-mu.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 
 app.use(bodyParser.json({ limit: "1mb" }));
