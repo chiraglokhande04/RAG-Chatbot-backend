f4c481df9ade3cfcc8f773df671b4d8c22ef5b1a
chiraglokhande04 <chiraglokhande14@gmail.com>
2025-11-02 02:25:14 +0530

    change cors

--- src/index.js
+++ src/index.js
@@ -17,10 +17,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://rag-chatbot-frontend-mu.vercel.app"
-  ],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
